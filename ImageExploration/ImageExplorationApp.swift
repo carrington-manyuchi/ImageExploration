e46fc07abd961e7b8070bd276ee19d50f081266c
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-12-16 07:32:34 +0200

    Initial Commit

---
+++ ImageExploration/ImageExplorationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageExplorationApp.swift
+//  ImageExploration
+//
+//  Created by Manyuchi, Carrington C on 2024/12/16.
+//
+
+import SwiftUI
+
+@main
+struct ImageExplorationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
